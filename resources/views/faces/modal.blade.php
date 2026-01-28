94d7ba23192020264afa5a396efd69a646b3c031
giuseppemorleo <morleogiuseppe@gmail.com>
2026-01-28 18:04:46 +0100

    aggiustamento del mitod mettendo il form

--- resources/views/faces/modal.blade.php
+++ resources/views/faces/modal.blade.php
@@ -1,11 +1,33 @@
 !-- Vertically centered scrollable modal -->
-<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-    <div class="mb-3">
-        <label for="name" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="name" placeholder="">
+<form  method="POST" action="" enctype="multipart/form-data">
+      @csrf
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" id="createpostmodal">
+        <div class="mb-3">
+            <label for="title" class="form-label">titlo del post</label>
+            <input type="text" class="form-control" name="title" id="title" placeholder="">
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">una breve descrizione</label>
+            <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="mood" class="form-label">stato d'animo</label>
+            <input type="text" class="form-control" id="mood" placeholder="" name="mood">
+        </div>
+        <div class="mb-3">
+            <label for="img" class="form-label">Immagine:</label>
+            <input type="file" class="form-control" id="img" placeholder="" name="img">
+        </div>
+           <div>
+            <button>Button</button>
+           </div>
     </div>
-    <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-</div>
+</form>
+
+
+
+
+
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <section class="row justify-content-center mt-3">
             <div class="d-flex gap-2 col-12 col-md-6 align-items-center form-post">
                 <img src="https://picsum.photos/300/200" alt="" class="icon-user">
-                <input class="form-control form-control-lg input-post" type="text" placeholder="A cosa stai pensando?" aria-label=".form-control-lg example">
+                <input class="form-control form-control-lg input-post" type="text" placeholder="A cosa stai pensando?" aria-label=".form-control-lg example" readonly data-bs-togle="modal" data-bs-target="#createpostmodal">
                 <i class="fa-regular fa-face-grin-tears fa-2x" style="color: #FFD43B;"></i>
                 <i class="fa-regular fa-file-image fa-2x" style="color: #054d00;"></i>
             </div>
