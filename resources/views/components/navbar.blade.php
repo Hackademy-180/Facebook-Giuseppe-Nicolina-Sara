3e7c2c007c5ff7b615201d7818cbb07277051834
Nicolina <nicolina.vesco@gmail.com>
2026-01-28 16:48:48 +0100

    Front-end

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,45 +1,52 @@
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg nav-custom">
   <div class="container-fluid">
-    <a class="navbar-brand" href=""><i class="fa-brands fa-facebook fa-2x "></i></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <form class="d-flex align-items-center" role="search">
-        {{-- <button>
+    <div class="d-flex">
+
+      <a class="navbar-brand" href=""><i class="fa-brands fa-facebook fa-2x "></i></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <form class="d-flex align-items-center" role="search">
+        <!-- {{-- <button>
           <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="search">
-              <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" stroke-width="1.333" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" stroke-width="1.333" stroke-linecap="round" stroke-linejoin="round"></path>
           </svg>
-      </button> --}}
+        </button> --}} -->
         
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <i class="fa-solid fa-magnifying-glass fa-1x" type="submit"></i>
-    </form>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route("welcome")}}"><i class="fa-solid fa-house"></i></a>
-        </li>
+      </form>
+    </div>
 
-        <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fa-brands fa-youtube"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fa-solid fa-people-group"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fa-solid fa-gamepad"></i></a>
-        </li>
-      </ul>
+
+    <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
+      <div class="d-flex gap-2">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-5">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route("welcome")}}"><i class="fa-solid fa-house fa-1x"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-brands fa-youtube fa-1x"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-solid fa-people-group fa-1x"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-solid fa-gamepad fa-1x"></i></a>
+          </li>
+        </ul>
+      </div>
+
         <div>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
             </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><a class="dropdown-item" href="#">Impostazioni e privacy</a></li>
+              <li><a class="dropdown-item" href="#">Assistenze e supporto</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#">Accessibilità</a></li>
             </ul>
           </li>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
     <h1> faaaaaacet</h1>
+    <main class="container">
+        <section class="row justify-content-center mt-3">
+            <div class="d-flex gap-2 col-12 col-md-6 align-items-center form-post">
+                <img src="https://picsum.photos/300/200" alt="" class="icon-user">
+                <input class="form-control form-control-lg input-post" type="text" placeholder="A cosa stai pensando?" aria-label=".form-control-lg example">
+                <i class="fa-regular fa-face-grin-tears fa-2x" style="color: #FFD43B;"></i>
+                <i class="fa-regular fa-file-image fa-2x" style="color: #054d00;"></i>
+            </div>
+        </section>
+    </main>
 </x-layout>
